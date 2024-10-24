8bfbc8389293f3b25e3bc32d780111c1f425229c
MoooYuuu <2020302131171@whu.edu.cn>
2024-10-24 11:49:25 +0800

    change the way to draw masks, enhancing its efficiency significantly

---
+++ extract_worker.py
@@ -0,0 +1,15 @@
+from PySide6.QtCore import QObject, Signal
+from SAM import extract_embedding 
+
+class ExtractWorker(QObject):
+    finished = Signal(object)  # 定义一个信号，用于传递结果（例如 predictor）
+
+    def __init__(self, image_path):
+        super().__init__()
+        self.image_path = image_path
+
+    def run(self):
+        # 在这里执行耗时的操作，例如提取 embedding
+        predictor = extract_embedding(self.image_path)
+        # 操作完成后发出信号，传递 predictor（提取结果）回主线程
+        self.finished.emit(predictor)
